30bb1a64c1464ddb7db8ae7ddd31e321721d8327
gitdeepaks <deepaksankhyan92@outlook.com>
2026-01-05 22:57:13 +0530

    connected clerk and convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+import { useMutation, useQuery } from "convex/react";
 import { Button } from "@/components/ui/button";
+import { api } from "../../convex/_generated/api";
 
 const Page = () => {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
   return (
-    <div>
-      <Button>Click Me</Button>
+    <div className="flex flex-col gap-2 p-4">
+      <Button
+        onClick={() =>
+          createProject({
+            name: "New Project123",
+          })
+        }
+      >
+        Add a label
+      </Button>
+      {projects?.map((task) => {
+        return (
+          <div className="border rounded p-2 gap-4" key={task._id}>
+            <p>{task.name}</p>
+            <p>Its completed: {task.ownerId}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/components/provider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useAuth,
+} from "@clerk/nextjs";
+import {
+  Authenticated,
+  AuthLoading,
+  ConvexReactClient,
+  Unauthenticated,
+} from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import React from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { AuthLoadingView } from "@/features/auth/components/authloading-view";
+import { UnauthenticatedView } from "@/features/auth/components/unauthenticated-view";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Authenticated>
+            <UserButton />
+            {children}
+          </Authenticated>
+          <Unauthenticated>
+            <UnauthenticatedView />
+          </Unauthenticated>
+          <AuthLoading>
+            <AuthLoadingView />
+          </AuthLoading>
+        </ThemeProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
